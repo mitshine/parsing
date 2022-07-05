fb8f74ac817d5b0446225ed928ce2a21c46d6218
mitshine <mitesh.khadgi2018@gmail.com>
2022-07-05 19:53:34 +0530

    Add files via upload

---
+++ read_file.rs
@@ -0,0 +1,22 @@
+use std::{
+    fs::File,
+    io::{prelude::*, BufReader},
+    path::Path,
+};
+
+fn lines_from_file(filename: impl AsRef<Path>) -> Vec<String> {
+    let file = File::open(filename).expect("no such file");
+    let buf = BufReader::new(file);
+    buf.lines()
+        .map(|l| l.expect("Could not parse line"))
+        .collect()
+}
+
+// ---
+
+fn main() {
+    let lines = lines_from_file("search.txt");
+    for line in lines {
+        println!("{:?}", line);
+    }
+}
